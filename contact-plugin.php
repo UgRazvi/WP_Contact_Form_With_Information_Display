fc689a8536ba31d4815d8074bea6f35762ed4e11
Muhammad Usman Gani Qureshi <69634693+UgRazvi@users.noreply.github.com>
2023-09-20 11:31:09 +0530

    Add files via upload

---
+++ contact-plugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 
+ * Plugin Name: Email Contact Plugin
+ * Description: A Very Handy & Beginner Friendly Plugin For My Custom Contact Form To Send Emails & Some Basic Details 
+ * Version: 1.0.0
+ * Text Domain: contact-plugin
+ * Author: Muhammed Usman Qureshi 
+ * Author URI: https://github.com/UgRazvi/
+ * 
+ */
+
+if( !defined('ABSPATH') )
+{
+      die('You cannot be here');
+}
+
+if( !class_exists('ContactPlugin') )
+{
+
+
+
+            class ContactPlugin{
+
+
+                  public function __construct()
+                  {
+
+                        define('MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+
+                        define('MY_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+                        require_once(MY_PLUGIN_PATH . 'vendor\autoload.php');
+
+                  }
+
+                  public function initialize()
+                  {
+                        include_once MY_PLUGIN_PATH . 'includes/utilities.php';
+
+                        include_once MY_PLUGIN_PATH . 'includes/options-page.php';
+
+                        include_once MY_PLUGIN_PATH . 'includes/contact-form.php';
+                  }
+
+
+            }
+
+            $contactPlugin = new ContactPlugin;
+            $contactPlugin->initialize();
+
+}
